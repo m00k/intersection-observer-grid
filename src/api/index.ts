d5292d1dd23cfed9376ba4771fbb22a97bc77df1
m00k <christian.bican@gmail.com>
2021-05-03 18:13:52 +0200

    refactor sample data generator

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,11 +4,11 @@ type Factory<T> = (_: unknown, index: number) => T;
 const rangeFn = <T>(l: number) => 
     (fn: Factory<T>): Array<T> => 
         (new Array(l))
-            .fill(0, 0)
-            .map(fn)
-;
+            .fill(0)
+            .map(fn);
 
 const CATEGORIES_TOTAL = 16;
+const MULTIPLIER = 3;
 const categoryFn: Factory<VM.Category> = (_, id) => ({ id, name: `Category_${id}` });
 export const categories: Array<VM.Category> = rangeFn<VM.Category>(CATEGORIES_TOTAL)(categoryFn);
 
@@ -16,9 +16,8 @@ const categoriesMedian = Math.ceil(CATEGORIES_TOTAL / 2);
 const categoryItemsFn = (category: VM.Category) => (_: unknown, id: number): VM.Item => ({ id, name: `Item_${id}`, category });
 export const itemsTotalByCategoryIndex: number[] = categories
     .map((_, i) => categoriesMedian - Math.abs((categoriesMedian - i - 1)))
-    .map(x => Math.max(3, x * 3))
-;
+    .map(x => Math.max(MULTIPLIER, x * MULTIPLIER));
+
 export const items: Array<VM.Item> = itemsTotalByCategoryIndex
-    .flatMap((l, i) => (new Array(l).fill(i, 0)))
-    .map((l, id) => categoryItemsFn(categories[l])(l, id))
-;
+    .flatMap((l, i) => (new Array(l).fill(i)))
+    .map((l, id) => categoryItemsFn(categories[l])(l, id));
