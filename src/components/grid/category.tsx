d9d8bd44922b101b626e15c65c37dfae2dc1cd02
m00k <christian.bican@gmail.com>
2021-05-05 09:57:17 +0200

    log category intersection values

--- src/components/grid/category.tsx
+++ src/components/grid/category.tsx
@@ -1,19 +1,44 @@
-import { FunctionComponent, h, ComponentChildren } from 'preact';
+import { ComponentChildren, FunctionComponent, h, RefObject } from 'preact';
+import { useRef } from 'preact/hooks';
 import * as VM from '../../api/model';
+import { useIntersectionObserver } from '../../components/grid/useIntersectionObserver';
 import style from './style.css';
 
 export interface CategoryProps {
-    children: ComponentChildren;
     category: VM.Category;
+    containerRef: RefObject<HTMLElement>;
+    children: ComponentChildren;
+}
+
+const log: (name: string) => IntersectionObserverCallback = name => (entries: IntersectionObserverEntry[]) => {
+    // TODO (cb):
+    // multiply the intersection ratio of a category with the number of items
+    // to get a more accurate result?
+    const entry = entries?.[0];
+    if (entry?.isIntersecting) {
+        const baseStyles = [
+            'color: #fff',
+            'background-color: #090',
+            'padding: 4px',
+            'border-radius: 2px'
+        ].join(';');
+        console.log(`%cintersection: ${Math.round(entry?.intersectionRatio * 100)}% ${name}`, baseStyles)
+    }
 }
 
 const Category: FunctionComponent<CategoryProps> = ({
-    children,
     category,
+    containerRef,
+    children
 }) => {
     const { name } = category;
+    const threshold = [0.1, 0.3, 0.5, 0.7, 0.9]
+    const options: IntersectionObserverInit = { root: containerRef.current, threshold }
+    const ref = useRef<HTMLElement>();
+    useIntersectionObserver(ref, log(name), options)
+
     return (
-        <section class={style.category}>
+        <section ref={ref} class={style.category}>
             <h3>{name}</h3>
             {children}
         </section>
--- src/routes/home/grid.tsx
+++ src/routes/home/grid.tsx
@@ -2,20 +2,15 @@ import { FunctionalComponent, h } from 'preact';
 import Grid from '../../components/grid';
 import { categories, categoriesWithItems } from '../../api';
 import { useRef } from 'preact/hooks';
-import { useIntersectionObserver } from '../../components/grid/useIntersectionObserver';
-
 
 const GridBlock: FunctionalComponent = () => {
     const containerRef = useRef<HTMLElement>()
-    const categoryRef = useRef<HTMLElement>()
-    const options: IntersectionObserverInit = { root: window.document.body }
-    useIntersectionObserver(categoryRef, () => console.log('############', 'intersect'), options)
 
     return (
         <Grid.Container ref={containerRef}>
             <Grid.Header {...{ categories }} />
             {categories.map(category => (
-                <Grid.Category key={category.id} {...{ category }}>
+                <Grid.Category key={category.id} {...{ category, containerRef }}>
                     <Grid.ItemList>
                         {categoriesWithItems[category.id].map(item => (
                             <Grid.ItemDetail key={item.id} {...{ item }} />)
