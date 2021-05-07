d3a18cda75c503ac0e0bf7256831db5ed5037fb0
m00k <christian.bican@gmail.com>
2021-05-07 09:18:57 +0200

    scroll nav into view on active index change

--- src/components/grid/nav.tsx
+++ src/components/grid/nav.tsx
@@ -1,6 +1,40 @@
 import { FunctionComponent, h } from 'preact';
+import { useEffect, useRef } from 'preact/hooks';
 import * as VM from '../../api/model';
 import style from './style.css';
+
+
+interface NavItemProps {
+    category: VM.Category;
+    index: number;
+    activeIndex: number;
+}
+
+const NavItem: FunctionComponent<NavItemProps> = ({
+    category,
+    index,
+    activeIndex,
+}) => {
+    const { name } = category;
+    const ref = useRef<HTMLAnchorElement>()
+    useEffect(() => {
+        const el = ref.current
+        const active = activeIndex === index
+        if (active && el?.parentElement) {
+            // TODO: only scroll into view when intersectionTarget !== self
+            el.parentElement.scrollTo({ left: el.offsetLeft, behavior: 'smooth' })
+        }
+    }, [activeIndex, index])
+    return (
+        <a
+            ref={ref}
+            href={`#${name}`} // TODO
+            style={{ color: activeIndex === index ? 'hotpink' : 'inherit' }}
+        >
+            {name}
+        </a>
+    )
+}
 
 export interface NavProps {
     categories: Array<VM.Category>;
@@ -12,17 +46,16 @@ const Nav: FunctionComponent<NavProps> = ({
     activeIndex,
 }) => {
     return (
-        <nav class={style.nav}>
-            {categories?.map(({name, id}, index) => (
-                <a
-                    key={id}
-                    href="#" 
-                    style={{color: activeIndex === index ? 'hotpink' : 'inherit'}}
-                    onClick={e => console.log(index, e)}>{name}
-                </a>
-            ))}
+        <nav
+            class={style.nav}
+        >
+            {categories?.map((category, index) => {
+                return (
+                    <NavItem key={category.id} {...{ category, index, activeIndex }} />
+                );
+            })}
         </nav>
-    );
+    )
 }
 
 export default Nav
