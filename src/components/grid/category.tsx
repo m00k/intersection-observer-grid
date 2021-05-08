8bc615e778b89b67dfb39eb182539efa5a0414ab
m00k <christian.bican@gmail.com>
2021-05-08 19:04:20 +0200

    use a single intersection observer to observe category nodes (wip)

--- src/components/grid/category.tsx
+++ src/components/grid/category.tsx
@@ -1,54 +1,25 @@
-import { ComponentChildren, FunctionComponent, h, RefObject } from 'preact';
-import { useEffect, useRef } from 'preact/hooks';
+import { ComponentChildren, FunctionComponent, h } from 'preact';
+import { forwardRef } from 'preact/compat';
 import * as VM from '../../api/model';
-import { useIntersectionObserver } from '../../components/grid/useIntersectionObserver';
-import { IntersectionTarget } from '../../routes/home/grid';
 import style from './style.css';
 
-export type CategoryIntersectionObserverCallback =
-    (index: number, entry: IntersectionObserverEntry) => void
-
 export interface CategoryProps {
-    index: number;
     category: VM.Category;
-    containerRef: RefObject<HTMLElement>;
-    activeIndex: number;
-    intersectionTarget: IntersectionTarget;
-    onCategoryIntersectionChanged: CategoryIntersectionObserverCallback;
     children: ComponentChildren;
 }
 
 const Category: FunctionComponent<CategoryProps> = ({
-    index,
     category,
-    containerRef,
-    activeIndex,
-    intersectionTarget,
-    onCategoryIntersectionChanged,
     children,
-}) => {
-    const { name } = category
-    const threshold = [0.1, 0.3, 0.5, 0.7, 0.9]
-    const options: IntersectionObserverInit = { root: containerRef.current, threshold }
-    const ref = useRef<HTMLElement>()
-    const handleIntersectionChanged: IntersectionObserverCallback = (entries: IntersectionObserverEntry[]) => {
-        const entry = entries[0]
-        onCategoryIntersectionChanged(index, entry)
-    }
-    useIntersectionObserver(ref, handleIntersectionChanged, options)
-    useEffect(() => {
-        if (intersectionTarget === 'category') return
-        if (activeIndex !== index) return
-        // TODO: 'smooth' not supported on safari
-        ref.current.scrollIntoView({behavior: 'smooth'})
-    }, [activeIndex, index, intersectionTarget])
+}, ref) => {
+    const { name, id } = category
 
     return (
-        <section ref={ref} class={style.category}>
+        <section ref={ref} class={style.category} data-id={id}>
             <h3>{name}</h3>
             {children}
         </section>
     );
 }
 
-export default Category
+export default forwardRef(Category)
--- src/routes/home/grid.tsx
+++ src/routes/home/grid.tsx
@@ -1,8 +1,7 @@
-import { FunctionalComponent, h } from 'preact';
-import Grid from '../../components/grid';
+import { Fragment, FunctionalComponent, h, RefCallback, RefObject } from 'preact';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'preact/hooks';
 import { categories, categoriesWithItems } from '../../api';
-import { useEffect, useRef, useState } from 'preact/hooks';
-import { CategoryIntersectionObserverCallback } from '../../components/grid/category';
+import Grid from '../../components/grid';
 
 const baseStyles = [
     'color: #fff',
@@ -30,54 +29,94 @@ const calcIndexWithMaxIntersectionRatio = (intersectionEntries: IntersectionObse
         }, 0)
 }
 
-const useHandleCategoryIntersectionChanged = (
-    handleActiveCategoryChanged: IndexChangedHandler
-): CategoryIntersectionObserverCallback => {
-    const categoryIntersectionsRef = useRef<IntersectionObserverEntry[]>([])
-    const categoryIntersections = categoryIntersectionsRef.current
-    return (index: number, entry: IntersectionObserverEntry): void => {
-        categoryIntersections[index] = entry
-        const nextActiveIndex = calcIndexWithMaxIntersectionRatio(categoryIntersections)
-        handleActiveCategoryChanged(nextActiveIndex)
-    }    
+const threshold = [0, 0.2, 0.4, 0.6, 0.8, 1];
+
+export const useIntersectionObserver = (
+    callback: IntersectionObserverCallback,
+    containerRef?: RefObject<Element>,
+    pOptions?: Omit<IntersectionObserverInit, 'root'>,
+): IntersectionObserver => {
+    return useMemo(() => {
+        console.log('############## observer init')
+        const options = { ...pOptions, root: containerRef?.current }
+        return new IntersectionObserver(callback, options)
+    // NOTE: adding pOptions will re-render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [callback, containerRef])
 }
 
 const GridBlock: FunctionalComponent = () => {
     const containerRef = useRef<HTMLElement>()
     const [activeIndex, setActiveIndex] = useState(0)
-    const [intersectionTarget, setIntersectionTarget] = useState<IntersectionTarget>('page')
-    const setIntersectionState = (index: number, target: IntersectionTarget): void => {
-        setActiveIndex(index)
-        setIntersectionTarget(target)
-    }
-    const cb: IndexChangedHandler = index => setIntersectionState(index, 'category')
-    const onCategoryIntersectionChanged = useHandleCategoryIntersectionChanged(cb)
-    const onNavigation: IndexChangedHandler = index => setIntersectionState(index, 'nav')
+    const categoryIntersectionsRef = useRef<IntersectionObserverEntry[]>()
+    const handleCategoryIntersection: IntersectionObserverCallback = useCallback((entries: IntersectionObserverEntry[]) => {
+        categoryIntersectionsRef.current = categoryIntersectionsRef.current || [...entries]
+        entries.forEach(e => {
+            const updateIndex = categoryIntersectionsRef.current.findIndex(ce => (ce.target as HTMLElement)?.dataset?.id === (e.target as HTMLElement)?.dataset?.id)
+            updateIndex > -1 && (categoryIntersectionsRef.current[updateIndex] = e)
+        })
+        const nextActiveIndex = calcIndexWithMaxIntersectionRatio(categoryIntersectionsRef.current)
+        setActiveIndex(nextActiveIndex)
+    }, [])
+    const observer = useIntersectionObserver(
+        handleCategoryIntersection,
+        containerRef,
+        { threshold },
+    );
+    const categoryRefCallback: RefCallback<HTMLDivElement> = useCallback(r => (r && observer.observe(r)), [observer])
+
+    // TODO
+    // disconnect while scrolling into view -> observer.disconnect() vs categoryIntersectionsRef.current.forEach(e => observer.unobserve(e.target))
+    // reconnect after nav scrolling is done -> categoryIntersectionsRef.current.forEach(e => observer.observe(e.target))
+
+    // const [intersectionTarget, setIntersectionTarget] = useState<IntersectionTarget>('page') // TODO: 'category' ?
+
+    // const setIntersectionState = (index: number, target: IntersectionTarget): void => {
+    //     if (target === 'nav') {
+    //         containerRef.current.children[index].scrollIntoView({behavior: 'smooth'})
+    //         setIntersectionTarget(target)
+    //         setActiveIndex(index)
+    //         // TODO: back to curr after timeout
+    //     }
+    //     if (target === 'category' && target === intersectionTarget) {
+    //         setIntersectionTarget(target)
+    //         setActiveIndex(index)
+    //     }
+
+    // }
+    // const cb: IndexChangedHandler = index => {
+    //     setIntersectionState(index, 'category')
+    // }
+    // const onCategoryIntersectionChanged = useHandleCategoryIntersectionChanged(cb)
+    // const onNavigation: IndexChangedHandler = index => setIntersectionState(index, 'nav')
 
     useEffect(() => { console.log(`%cactive category index: ${activeIndex}`, baseStyles) }, [activeIndex])
+    useEffect(() => { console.log(`observer: ${observer}`, baseStyles) }, [observer])
+    useEffect(() => { console.log(`containerRef: ${containerRef}`, baseStyles) }, [containerRef])
+    useEffect(() => { console.log(`handleCategoryIntersection:`, baseStyles) }, [handleCategoryIntersection])
+    useEffect(() => { console.log(`categoryRefCallback:`, baseStyles) }, [categoryRefCallback])
 
     return (
-        <Grid.Container ref={containerRef}>
-            <Grid.Nav {...{ categories, activeIndex, intersectionTarget, onNavigation }} />
-            {categories.map((category, index) => (
-                <Grid.Category
-                    key={category.id}
-                    {...{
-                        index,
-                        category,
-                        containerRef,
-                        activeIndex,
-                        intersectionTarget,
-                        onCategoryIntersectionChanged,
-                    }}>
-                    <Grid.ItemList>
-                        {categoriesWithItems[category.id].map(item => (
-                            <Grid.ItemDetail key={item.id} {...{ item }} />)
-                        )}
-                    </Grid.ItemList>
-                </Grid.Category>
-            ))}
-        </Grid.Container>
+        <Fragment>
+            <Grid.Nav {...{ categories, activeIndex, intersectionTarget: null, onNavigation: Function.prototype }} />
+            <Grid.Container ref={containerRef}>
+                {categories.map(category => (
+                    <Grid.Category
+                        key={category.id}
+                        ref={categoryRefCallback}
+                        {...{
+                            category,
+                            containerRef,
+                        }}>
+                        <Grid.ItemList>
+                            {categoriesWithItems[category.id].map(item => (
+                                <Grid.ItemDetail key={item.id} {...{ item }} />)
+                            )}
+                        </Grid.ItemList>
+                    </Grid.Category>
+                ))}
+            </Grid.Container>
+        </Fragment>
     );
 };
 
